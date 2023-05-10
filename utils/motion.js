e31e2e48f18742dae832ccfb14e845193438a6ec
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-05-10 21:38:30 +1000

    Add motion props for Scroll Progress component

--- utils/motion.js
+++ utils/motion.js
@@ -159,3 +159,32 @@ export const footerVariants = {
     },
   },
 };
+
+export const animateOnScroll = {
+  initial: { 
+    y: 100, 
+    opacity: 0 },
+  animate: { 
+    y: 0, 
+    opacity: 1, 
+    transition: { 
+      duration: 0.6 
+    }, 
+  },
+  exit: { 
+    y: 100, 
+    opacity: 0, 
+    transition: { 
+      duration: 0.6 
+    }, 
+  },
+  whileHover: { 
+    scale: 1.2, 
+    transition: { 
+      duration: 0.2 
+    }, 
+  },
+  whileTap: { 
+    scale: 1 
+  },
+}
