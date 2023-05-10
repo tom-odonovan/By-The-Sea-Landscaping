21e6f87a666f0c8df319ba6c7616bb4b29369f54
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-05-10 21:41:26 +1000

    Add scrollProgress component to page

--- app/page.js
+++ app/page.js
@@ -1,30 +1,41 @@
-import { Footer, Navbar } from '../components';
+'use client';
+
+import { Navbar } from '../components';
+import { Footer } from '../components';
+import ProgressCircle from '../components/ProgressCircle';
 import { Hero, About, Explore, Feedback, WhatsNew, World } from '../sections';
 import Blog from '../sections/Blog';
 import WhyChooseUs from '../sections/WhyChooseUs';
 import GetInTouch from '../sections/GetInTouch';
 
+import { useRef } from 'react';
 
 
-const Page = () => (
-  <div className='bg-primary-white overflow-hidden font-sans'>
-    <Navbar />
-    <Hero />
-    <div className='relative'>
-      <About />
-      <WhyChooseUs />
+const Page = () => {
+
+  const pageRef = useRef();
+
+  return (
+    <div className='bg-primary-white overflow-hidden font-sans' ref={pageRef}>
+      <Navbar />
+      <Hero />
+      <div className='relative'>
+        <About />
+        <WhyChooseUs />
+      </div>
+      <div className='relative'>
+        <Explore />
+        <WhatsNew />
+      </div>
+      <div className='relative'>
+        <Blog />
+        <Feedback />
+        <GetInTouch />
+      </div>
+      <ProgressCircle pageRef={pageRef} />
+      <Footer />
     </div>
-    <div className='relative'>
-      <Explore />
-      <WhatsNew />
-    </div>
-    <div className='relative'>
-      <Blog />
-      <Feedback />
-      <GetInTouch />
-    </div>
-    <Footer />
-  </div>
-);
+  )
+};
 
 export default Page;
