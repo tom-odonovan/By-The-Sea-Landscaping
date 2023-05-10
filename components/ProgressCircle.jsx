92766fe08a58594300c6b7630a3c08f1ce4e51cf
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-05-10 21:39:39 +1000

    Create component to track scroll position and scroll back to top

---
+++ components/ProgressCircle.jsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { useState, useLayoutEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import { BsArrowUp } from 'react-icons/bs';
+import { animateOnScroll } from '../utils/motion';
+
+
+const ProgressCircle = ({ pageRef }) => {
+
+    const diameter = 50;
+    const strokeWidth = 3;
+    const radius = diameter / 2 - strokeWidth / 2;
+    const circumference = Math.PI * radius * 2;
+
+    const [progress, setProgress] = useState(0);
+    const position = Math.max(1 - progress, 0);
+
+
+    useLayoutEffect(() => {
+        const updateHeight = () => {
+            if (!pageRef.current) return;
+            const { height } = pageRef.current.getBoundingClientRect();
+            setProgress(window.scrollY / (height - window.innerHeight));
+        };
+
+        updateHeight();
+
+        window.addEventListener("scroll", updateHeight);
+        return () => {
+            window.removeEventListener("scroll", updateHeight);
+        };
+    }, []);
+
+    const scrollToTop = () => {
+        console.log('success')
+        document.documentElement.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+
+    return (
+
+        <AnimatePresence>
+            {position < 0.95 && (
+                <motion.button
+                    onClick={scrollToTop}
+                    className='floating-element fixed bottom-[50px] right-[50px] z-10 cursor-pointer'
+                    {...animateOnScroll}
+                >
+                    <svg
+                        viewBox="0 0 50 50"
+                        width="60px"
+                        height="60px"
+                        className="relative z-1 -rotate-90"
+                    >
+                        <circle
+                            cx={diameter / 2}
+                            cy={diameter / 2}
+                            r={radius}
+                            stroke="#3CAB80"
+                            strokeWidth={strokeWidth}
+                            fill="transparent"
+                            style={{ strokeDasharray: circumference, strokeDashoffset: circumference * position }}
+                        />
+                    </svg>
+
+                    <div className="absolute bottom-0 right-0 w-full h-full rounded-full border-4 border-opacity-50 z-[-1] flex items-center justify-center">
+                        <BsArrowUp size={29} color='#3CAB80' />
+                    </div>
+                </motion.button>
+            )}
+        </AnimatePresence>
+
+    );
+};
+
+export default ProgressCircle;
