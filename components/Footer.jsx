7bd1052184b8dd99fbb7927d715d3a007a99c8c9
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-05-22 21:01:20 +1000

    Create Footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,32 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { socials } from '../constants';
 import styles from '../styles';
+import { footerVariants } from '../utils/motion';
 
 const Footer = () => (
-  <footer className={`${styles.xPaddings} py-8 relative bg-palette-1 z-5 opacity-90`}
-  >
-    {/* <div className='absolute w-[50%] inset-0 gradient-01' /> */}
-    <div className={`max-w-[2400px] mx-auto flex justify-between gap-8 h-[200px] items-center`}>
+  <footer className={`py-8 relative bg-palette-1 z-5 opacity-90`} >
+    <motion.div 
+      variants={footerVariants}
+      initial='hidden'
+      whileInView='show'
+      className={`max-w-[1700px] mx-auto flex flex-col gap-8 items-center justify-between`}>
       
-    </div>
+      <div>
+        <div className='flex gap-4'>
+          {socials.map(({ name, url }) => (
+            <img
+              key={name}
+              src={url}
+              alt={name}
+              className='w-[24px] h-[24px] object-contain cursor-pointer'
+            />
+          ))}
+        </div>
+      </div>
+      <p className='text-white text-[12px]'>Copyright © By The Sea Landscaping 2023 | Website by <b>Tom O'Donovan</b></p>
+    </motion.div>
   </footer>
 );
 
