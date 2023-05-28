fe1d0dfa3c196454b02a186a3752ecbe839cd001
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-05-28 15:33:20 +1000

    Update FormInput component

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { staggerContainer, fadeIn } from '../../utils/motion'
 import styles from '../../styles';
@@ -8,6 +9,17 @@ import FormInput from './FormInput';
 import { IoSendSharp } from 'react-icons/io5';
 
 const ContactForm = () => {
+
+    const [formData, setFormData] = useState({});
+    console.log(formData);
+
+    const handleFormChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => ({
+            ...prevState,
+            [name]: value
+        }));
+    }
 
     return (
         <motion.div
@@ -32,40 +44,50 @@ const ContactForm = () => {
                     Whether you have questions, inquiries, or simply want to chat about your landscaping needs, we're here to help. Just fill out the form below, and we'll get back to you as soon as possible. We can't wait to bring your outdoor dreams to life!
                 </p>
 
-                <form>
-                    <div className='flex flex-row flex-wrap py-12 gap-6'>
+                <form method='POST'>
+                    <div className='flex flex-row flex-wrap py-12 gap-6 gap-y-10'>
                         <FormInput 
                             id='name'
                             type='text'
                             name='name'
                             placeholder="You're Name"
+                            value={formData.name}
+                            handleChange={handleFormChange}
                         />
                         <FormInput 
                             id='email'
                             type='email'
                             name='email'
                             placeholder="Email"
+                            value={formData.email}
+                            handleChange={handleFormChange}
                         />
                         <FormInput 
                             id='suburb'
                             type='text'
                             name='suburb'
                             placeholder="Suburb"
+                            value={formData.suburb}
+                            handleChange={handleFormChange}
                         />
                         <FormInput 
                             id='phone'
                             type='text'
                             name='phone'
                             placeholder="Phone"
+                            value={formData.phone}
+                            handleChange={handleFormChange}
                         />
                         <FormInput 
                             id='message'
                             type='textarea'
                             name='message'
                             placeholder="Message"
+                            value={formData.message}
+                            handleChange={handleFormChange}
                         />
-                        <button className={`${styles.flexCenter} flex-row gap-2 mt-6 bg-palette-2 border-[1px] border-palette-2 text-white text-lg rounded-lg px-8 py-4 w-full hover:bg-white hover:text-palette-2 active:scale-95 transition duration-300`}>SUBMIT
-                            <IoSendSharp className='ml-2' />
+                        <button className={`${styles.flexCenter} flex-row gap-2 mt-6 font-sans bg-palette-2 border-[2px] border-palette-2 text-white text-lg rounded-lg px-8 py-4 w-full hover:bg-white hover:text-palette-2 active:scale-95 transition duration-300`}>SEND
+                            {/* <IoSendSharp className='ml-2' /> */}
                         </button>
 
                     </div>
--- components/contact/FormInput.jsx
+++ components/contact/FormInput.jsx
@@ -7,24 +7,28 @@ const FormInput = ({ label, type, name, placeholder, value, handleChange }) => {
         <div className='flex flex-col flex-grow gap-1'>
             { label && <label htmlFor={name}>{label}</label> }
             { type === 'textarea' ? (
-                <textarea
-                    id={name}
-                    name={name}
-                    placeholder={placeholder}
-                    value={value}
-                    onChange={handleChange}
-                    className="appearance-none bg-gray-100/50 text-[14px] border rounded w-full min-h-[150px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                />
+                <label>
+                    <textarea
+                        id={name}
+                        name={name}
+                        value={value}
+                        onChange={handleChange}
+                        className="appearance-none bg-gray-100/50 text-[14px] border rounded w-full min-h-[150px] py-2 px-3 text-gray-700 leading-tight focus:outline-palette-2 focus:shadow-outline"
+                    />
+                    <span className={`absolute left-2 top-2 px-2 h-[25px] text-[14px] transition duration-200 input-textarea ${value ? 'text-palette-2 bg-opacity-0 transform -translate-y-10 -translate-x-4' : ''}`}>{placeholder}</span>
+                </label>
             ) : (
-                <input
-                    id={name}
-                    name={name}
-                    type={type}
-                    placeholder={placeholder}
-                    value={value}
-                    onChange={handleChange}
-                        className="appearance-none bg-gray-100/50 text-[14px] border rounded w-full p-4 text-gray-700 leading-tight focus:outline-palette-2 focus:shadow-outline"
-                />
+                <label className="relative">
+                    <input
+                        id={name}
+                        name={name}
+                        type={type}
+                        value={value}
+                        onChange={handleChange}
+                        className="appearance-none text-[14px] bg-gray-100/50 border rounded w-full p-4 text-gray-700 leading-tight focus:outline-palette-2 focus:shadow-md"
+                    />
+                        <span className={`absolute left-2 top-2 px-2 h-[25px] text-[14px] transition duration-200 input-text ${value ? 'text-palette-2 bg-opacity-0 transform -translate-y-10 -translate-x-4' : ''}`}>{placeholder}</span>
+                </label>
             )
             }
         </div>
