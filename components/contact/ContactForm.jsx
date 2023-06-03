f184298e97a18f1489c04b3aa31fe4430cdaa313
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-06-03 20:35:43 +1000

    Fix contact form validation

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import styles from '../../styles';
 import FormInput from './FormInput';
 import { IoSendSharp } from 'react-icons/io5';
-import { useFormik } from 'formik'; 
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 const ContactForm = () => {
@@ -19,7 +19,7 @@ const ContactForm = () => {
         }));
     }
 
-    const formik = useFormik({
+    const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
         initialValues: {
             name: '',
             email: '',
@@ -27,16 +27,18 @@ const ContactForm = () => {
             phone: '',
             message: '',
         },
-        onSubmit: values => {
-            console.log(values);    
+        onSubmit: (values, actions) => {
+            console.log(values)
+            actions.resetForm()
         },
         validationSchema: Yup.object({
-            name: Yup.string()
-                .max(20, 'Name must be 20 characters')
-                .required('Required'),
-            email: Yup.string()
-                .email('Invalid email address')
-                .required('Required *'),
+            name: Yup
+                .string()
+                .required('Please enter your name'),
+            email: Yup
+                .string()
+                .email('Please enter a vaild email')
+                .required('Please enter your email'),
         })
     })
 
@@ -47,48 +49,51 @@ const ContactForm = () => {
                 Whether you have questions, inquiries, or simply want to chat about your landscaping needs, we're here to help. Just fill out the form below, and we'll get back to you as soon as possible. We can't wait to bring your outdoor dreams to life!
             </p>
 
-            <form method='POST' onSubmit={formik.handleSubmit}>
+            <form method='POST' onSubmit={handleSubmit}>
                 <div className='flex flex-row flex-wrap pt-12 gap-6 gap-y-10'>
-                    <FormInput 
+                    <FormInput
                         id='name'
                         type='text'
                         name='name'
-                        placeholder={`${formik.errors.name ? formik.errors.name : "You're Name"}`}
-                        value={formik.values.name}
-                        handleChange={formik.handleChange}
-                        errorStyles={formik.errors.name ? 'text-red-500' : ''}
+                        placeholder={`${errors.name && touched.name ? errors.name : "Your name"}`}
+                        value={values.name}
+                        handleChange={handleChange}
+                        error={errors.name}
+                        touched={touched.name}
                     />
-                    <FormInput 
+                    <FormInput
                         id='email'
                         type='email'
                         name='email'
-                        placeholder="Email"
-                        value={formik.values.email}
-                        handleChange={formik.handleChange}
+                        placeholder={`${errors.email && touched.email ? errors.email : "Your email"}`}
+                        value={values.email}
+                        handleChange={handleChange}
+                        error={errors.email}
+                        touched={touched.email}
                     />
-                    <FormInput 
+                    <FormInput
                         id='suburb'
                         type='text'
                         name='suburb'
                         placeholder="Suburb"
-                        value={formik.values.suburb}
-                        handleChange={formik.handleChange}
+                        value={values.suburb}
+                        handleChange={handleChange}
                     />
-                    <FormInput 
+                    <FormInput
                         id='phone'
                         type='text'
                         name='phone'
                         placeholder="Phone"
-                        value={formik.values.phone}
-                        handleChange={formik.handleChange}
+                        value={values.phone}
+                        handleChange={handleChange}
                     />
-                    <FormInput 
+                    <FormInput
                         id='message'
                         type='textarea'
                         name='message'
                         placeholder="Message"
-                        value={formik.values.message}
-                        handleChange={formik.handleChange}
+                        value={values.message}
+                        handleChange={handleChange}
                     />
                     <button type='submit' className={`${styles.flexCenter} flex-row gap-2 font-sans bg-palette-2 border-[2px] border-palette-2 text-white text-lg rounded-lg px-8 py-4 w-full hover:bg-white hover:text-palette-2 active:scale-95 transition duration-300`}>SEND
                         {/* <IoSendSharp className='ml-2' /> */}
--- components/contact/FormInput.jsx
+++ components/contact/FormInput.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 
-const FormInput = ({ label, type, name, placeholder, value, handleChange, errorStyles }) => {
+const FormInput = ({ label, type, name, placeholder, value, handleChange, error, touched }) => {
 
     return (
         <div className='flex flex-col flex-grow gap-1'>
@@ -13,9 +13,9 @@ const FormInput = ({ label, type, name, placeholder, value, handleChange, errorS
                         name={name}
                         value={value}
                         onChange={handleChange}
-                        className="appearance-none bg-gray-100/50 text-[14px] border-[1px] border-gray-300 rounded w-full min-h-[150px] py-2 px-3 text-gray-700 leading-tight focus:border-palette-2 focus:ring-palette-2 focus:shadow-md"
+                        className="appearance-none bg-gray-100/50 text-[14px] border-[1px] border-gray-300 rounded w-full min-h-[150px] p-4 text-gray-700 leading-tight focus:border-palette-2 focus:ring-palette-2 focus:shadow-md"
                     />
-                    <span className={`absolute left-2 top-2 px-2 h-[25px] text-[14px] text-gray-400 transition duration-200 input-textarea ${value ? 'text-palette-2 bg-none transform -translate-y-10 -translate-x-4' : ''} ${errorStyles}`}>{placeholder}</span>
+                    <span className={`absolute left-2 top-2 px-2 h-[25px] text-[14px] text-gray-400 transition duration-200 input-textarea ${value ? 'font-bold text-gray-500 bg-none transform -translate-y-10 -translate-x-4' : ''}`}>{placeholder}</span>
                 </label>
             ) : (
                 <label className="relative">
@@ -25,9 +25,9 @@ const FormInput = ({ label, type, name, placeholder, value, handleChange, errorS
                         type={type}
                         value={value}
                         onChange={handleChange}
-                            className="appearance-none text-[14px] bg-gray-100/50 border-[1px] border-gray-300 rounded w-full p-4 text-gray-700 leading-tight focus:border-palette-2 focus:ring-palette-2 focus:shadow-md"
+                        className={`appearance-none text-[14px] bg-gray-100/50 border-[1px] border-gray-300 rounded w-full p-4 text-gray-700 leading-tight ${error && touched ? 'focus:ring-red-500 focus:border-red-500 border-red-500' : 'focus:border-palette-2 focus:ring-palette-2 focus:shadow-md'}`}
                     />
-                        <span className={`absolute left-2 top-2 px-2 h-[25px] text-[14px] text-gray-400 transition duration-200 input-text ${value ? 'text-palette-2 bg-none transform -translate-y-10 -translate-x-4' : ''} ${errorStyles ? errorStyles : ''}`}>{placeholder}</span>
+                        <span className={`absolute left-2 top-2 px-2 h-[25px] text-[14px] text-gray-400 transition duration-200 input-text ${value ? 'font-bold text-gray-500 bg-none transform -translate-y-10 -translate-x-4' : ''} ${error && touched ? 'font-normal text-red-500 transform -translate-y-10 -translate-x-4' : ''}`}>{placeholder}</span>
                 </label>
             )
             }
